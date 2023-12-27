3c0e20e0f1aa7eae3f8b3664c6756977e8210949
Morteza-Abbaszadeh <morteza.at1996@gmail.com>
2023-12-27 19:42:35 +0330

    store applicatin in database

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import sqlite3
-from flask import  Flask ,render_template , jsonify
-from database import load_jobs_from_db
+from flask import  Flask ,render_template , jsonify ,request
+from database import load_jobs_from_db,add_application_to_db
+
 app = Flask(__name__)
-
-
 @app.route('/')
 def hello_world():
     JOBS = load_jobs_from_db()
@@ -20,10 +18,21 @@ def show_jobs(id):
     for job in JOBS:
         if job['id'] == int(id):
             select_job = job
+            print(select_job)
     if select_job == None:
         return "Not Found" , 404
     else :
         return render_template('jobpage.html' ,job= select_job)
+@app.route("/jobs/<id>/apply", methods = ['post'] )
+def apply_to_job(id):
+    data = request.form
+    JOBS = load_jobs_from_db()
+    select_job = None
+    for job in JOBS:
+        if job['id'] == int(id):
+            select_job = job
+    add_application_to_db(select_job,data)
+    return render_template('app_submit.html' , application = data,job= select_job)
 
 if __name__ == "__main__":
     app.run(debug = True)
--- database.py
+++ database.py
@@ -4,7 +4,8 @@ def creat(database_name = 'database.db'):
     
     conn = sqlite3.connect(database_name)
     cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE jobs 
+    cursor.execute('''
+                CREATE TABLE jobs 
                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title VARCHAR(250) NOT NULL,
                 location VARCHAR(250) NOT NULL,
@@ -38,3 +39,28 @@ def load_jobs_from_db():
         job_dict = dict(zip(columns_name, row))
         jobs.append(job_dict)
     return jobs 
+def add_application_to_db(job_id ,application ):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS application(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   job_id INT not null,
+                   full_name VARCHAR(250) NOT NULL,
+                   email VARCHAR(250) NOT NULL,
+                   linkedin_url VARCHAR(500)                   
+    )""")
+    conn.commit()
+    conn.close()
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""insert into application(
+                   job_id, full_name, email, linkedin_url) values(?,?,?,?)""",
+                     (job_id['id'], application['full_name'], application['email'],
+                       application['Linkedin']))
+    
+
+    conn.commit()
+    conn.close()
